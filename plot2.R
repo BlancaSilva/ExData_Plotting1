3f4da79bb2304d0c1f96a30a51e1a779ae1d3b54
BlancaSilva <blanca.silva.fdez@gmail.com>
2017-03-04 12:10:18 +0100

    plot2

---
+++ plot2.R
@@ -0,0 +1,26 @@
+plot.new
+library(dplyr)
+library(data.table)
+library(lubridate)
+library(graphics)
+library(grDevices)
+
+alldata<-fread("household_power_consumption.txt", sep="auto",na.strings="?", stringsAsFactors=FALSE, skip=0, autostart = TRUE)
+EPC<-filter(alldata, alldata$Date=="1/2/2007"| alldata$Date=="2/2/2007")
+rm(alldata)
+totdate<-paste(EPC$Date,EPC$Time, sep=" ")
+totdate2<-dmy_hms(totdate)
+timehms<-hms(EPC$Time)
+time<-period_to_seconds(timehms)
+plotdata2<-cbind(time,EPC$Global_active_power)
+plotdata2<-as.data.frame(plotdata2)
+plotdata<-filter(plotdata2, complete.cases(plotdata2))
+names(plotdata)<-c("Time","Global_active_power")
+
+png(file="plot2.png",width=480,height=480)
+par(bg=NA)
+plot(totdate2, plotdata$Global_active_power, type="l", xlab="", ylab = "Global Active Power (kiloWatts)")
+dev.off()
+#  in case of incorrect language on the axis, run:
+#Sys.putenv(LANGUAGE="en")
+#Sys.setenv("LANGUAGE"="En")
