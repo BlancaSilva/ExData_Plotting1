e2aebe34a1252f7e93f32dd64795bdaf78a3248b
BlancaSilva <blanca.silva.fdez@gmail.com>
2017-03-04 13:03:55 +0100

    plot1

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 library(dplyr)
 library(data.table)
-#first we reed all the data and select the ones of the dates of our study (EPC) removing the rest
+#first we read all the data (that must be unzipped in the wd) and select the ones of the dates of our study (EPC) removing the rest
 alldata<-fread("household_power_consumption.txt", sep="auto",na.strings="?", stringsAsFactors=FALSE, skip=0, autostart = TRUE)
 EPC<-filter(alldata, alldata$Date=="1/2/2007"| alldata$Date=="2/2/2007")
 rm(alldata)
